ce7cc19ad4fc3dd313a53406c6e5f50e1ed0d204
Mehmet Demir <88983987+jackalkarlos@users.noreply.github.com>
2023-03-23 14:19:24 +0300

    Update ASCII.py

--- INTRODUCTION TO CRYPTOHACK/ASCII.py
+++ INTRODUCTION TO CRYPTOHACK/ASCII.py
@@ -1,3 +1,5 @@
 ords = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+#An ascii list
 print("Here is your flag:")
+#Get all values one by one, convert to char and merge. 
 print("".join(chr(o) for o in ords))

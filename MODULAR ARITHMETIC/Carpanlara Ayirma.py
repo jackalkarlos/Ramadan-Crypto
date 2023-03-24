5d8ff0432c425911842ab7b7bd277d29243f7ab9
Mehmet Demir <88983987+jackalkarlos@users.noreply.github.com>
2023-03-24 13:10:07 +0300

    Add files via upload

---
+++ MODULAR ARITHMETIC/Carpanlara Ayirma.py
@@ -0,0 +1,16 @@
+def carpanlar(a):
+    carpanlar=[]
+    carpan=2
+    if a == 0 or a < -1:
+        print("dude, what u trying? lol.")
+    else:
+        while a > 1:
+            if a % carpan == 0:
+                carpanlar.append(carpan)
+                a /= carpan
+            else:
+                carpan += 1
+        carpanlar.insert(0,1)
+        return carpanlar
+
+print(carpanlar(int(input("Çarpanını Almak istediğiniz Sayıyı Giriniz: "))))

122c57c6056cf5e5f5c0fb18fad92dc4eaeb2c57
Mehmet Demir <88983987+jackalkarlos@users.noreply.github.com>
2023-03-24 11:08:47 +0300

    Add files via upload

--- MODULAR ARITHMETIC/Greatest Common Divisor.py
+++ MODULAR ARITHMETIC/Greatest Common Divisor.py
@@ -1,1 +1,13 @@
-#şiddet ve hack arasında gitgellerim var
+def gcd(a, b):
+    while a != b:
+        if a > b:
+            a = a-b
+        else:
+            b = b-a
+    return a
+print(gcd(66528,52920))
+
+#or
+
+import math
+print(math.gcd(66528,52920))

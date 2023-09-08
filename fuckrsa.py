7a6bc3dc5b4cb1bec55ea6957f67bf427b3f0c79
Mehmet Demir <88983987+jackalkarlos@users.noreply.github.com>
2023-09-08 23:33:01 +0300

    Create fuckrsa.py

---
+++ fuckrsa.py
@@ -0,0 +1,25 @@
+import random
+import math
+from Crypto.Util.number import getPrime, bytes_to_long, long_to_bytes
+from sympy import mod_inverse
+
+def calculate_d(e, tot):
+        d = mod_inverse(e, tot)
+        return d
+
+def encrypt(plaintext):
+        p = getPrime(2048)
+        q = getPrime(2048)
+        n = p * q
+        tot = (p-1) * (q-1)
+        e = random.randrange(2, tot)
+        while math.gcd(e, tot) != 1:
+                e = random.randrange(2, tot)
+        d = calculate_d(e, tot)
+        intstr = bytes_to_long(plaintext.encode())
+        encrypted = pow(intstr,e,n)
+        return encrypted, d, n
+
+def decrypt(cipherint, d, n):
+        m = pow(cipherint, d, n)
+        return long_to_bytes(m).decode()
